d530fc635224ea65ff57108c503f6fa945fb8d39
Subinramachandran <subinramachandran28@gmail.com>
2026-08-25 04:54:09 +0530

    delete button added

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -4,54 +4,57 @@ const Dashboard = () => {
   const [inputValue, setInputValue] = useState("")
   const [items, setItems] = useState([])
 
-  const addItem = () => {
-    if (inputValue.trim() === "") return
-
+  const addNewItem = () => {
+    if (inputValue === "") return
     const newItem = {
       id: Date.now(),
       name: inputValue
     }
-    setItems(prevItems => [...prevItems, newItem])
+    setItems([...items, newItem])
     setInputValue("")
   }
-
+  const deleteItem = (id) => {
+      setItems(items.filter((item) => item.id !== id))
+  }
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center items-start pt-20">
-      <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-6">
-        <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
-          Todo List
+    <div className="min-h-screen bg-gray-100 flex justify-center items-start pt-16">
+      <div className="bg-white w-full max-w-md p-6 rounded-xl shadow-lg cursor-pointer">
+        <h1 className="text-2xl font-bold text-gray-800 mb-6">
+          My Dashboard
         </h1>
-
-        <div className="flex gap-3">
+        <div className="flex gap-2">
           <input
             type="text"
             value={inputValue}
+            placeholder="Enter an item"
             onChange={(e) => setInputValue(e.target.value)}
-            placeholder="Enter a task..."
-            className="flex-1 border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="flex-1 border border-gray-300 rounded-lg px-4 py-2 outline-none focus:ring-1 focus:ring-blue-500"
           />
-
           <button
-            onClick={addItem}
+            onClick={addNewItem}
             className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 transition cursor-pointer"
           >
-            Add
+            Add Item
           </button>
         </div>
-
-        <ul className="mt-6 space-y-3">
+        <ul className="mt-6 space-y-2">
           {items.map((item) => (
             <li
               key={item.id}
-              className="bg-gray-50 border rounded-lg px-4 py-3 shadow-sm"
+              className="bg-gray-50 border border-gray-200 px-4 py-3 rounded-lg text-gray-700 flex justify-between items-center"
             >
-              {item.name}
+              <span>{item.name}</span>
+              <button
+                onClick={() => deleteItem(item.id)}
+                className="bg-red-500 text-white px-3 py-1 rounded-lg hover: bg-red-600 cursor-pointer"
+              >
+                Delete
+              </button>
             </li>
           ))}
         </ul>
       </div>
     </div>
-  );
-};
-
+  )
+}
 export default Dashboard
